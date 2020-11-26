d81e55587fc3fd4fdcce6c821c35d7c8ee0de068
joaoback <joaoback47@gmail.com>
2020-11-25 23:41:05 -0300

    reajust

--- Source/Exercise01.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main(){
-	stdout.write('Type a number: ');
-	int value = int.tryParse(stdin.readLineSync());
-	int predecessor = value - 1;
-	print('The predecessor of ' + value.toString() + ' is ' + predecessor.toString());
-}
